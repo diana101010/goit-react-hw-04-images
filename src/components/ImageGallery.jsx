fc499c95b2e5bf457a2d04824353e7517e5fd972
diana101010 <150705147+diana101010@users.noreply.github.com>
2024-10-20 17:14:25 +0300

    16

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,37 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ImageGalleryItem from './ImageGalleryItem';
 import styles from './ImageGallery.module.css';
 
 const ImageGallery = ({ images, onImageClick }) => {
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  const handleImageClick = image => {
-    setSelectedImage(image);
-    if (onImageClick) {
-      onImageClick(image);
-    }
-  };
-
   return (
-    <>
-      <ul className={styles.ImageGallery}>
-        {images.map(image => (
-          <ImageGalleryItem
-            key={image.id}
-            image={image}
-            onImageClick={() => handleImageClick(image)}
-            className={`${styles.ImageGalleryItem} ${
-              styles[`Item-${image.id}`]
-            }`}
-          />
-        ))}
-      </ul>
-      {selectedImage && (
-        <div>
-          <p>Selected Image: {selectedImage.title}</p>
-        </div>
-      )}
-    </>
+    <ul className={styles.ImageGallery}>
+      {images.map(image => (
+        <ImageGalleryItem
+          key={image.id}
+          image={image}
+          onImageClick={onImageClick}
+          className={styles.ImageGalleryItem - image}
+        />
+      ))}
+    </ul>
   );
 };
 
